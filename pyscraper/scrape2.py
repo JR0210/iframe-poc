6113e15e07f4b7c2666d9db5f39f15ec7c1f4ed5
Jacob <robinson.jacob.e@gmail.com>
2021-12-10 02:38:25 +0000

    added dateparser for fuzzy date strings

--- pyscraper/scrape2.py
+++ pyscraper/scrape2.py
@@ -4,12 +4,17 @@ import re
 import lxml
 from functools import reduce
 import itertools
+from dateutil.parser import parse
+
+import codecs
+
+data = codecs.open("productPage.html", 'r', 'utf-8')
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-channel = requests.get(
-    'https://dreamcargiveaways.co.uk/current-competitions/audi-rs6-or-porsche-911-carrera-s-or-100000-tax-free/', timeout=10, headers=headers).text
+# channel = requests.get(
+#     'https://dreamcargiveaways.co.uk/current-competitions/audi-rs6-or-porsche-911-carrera-s-or-100000-tax-free/', timeout=10, headers=headers).text
 
 productSelector = {
     "container": '',
@@ -29,7 +34,7 @@ passedProps = {
     "timeRemaining": '3hrs',
 }
 
-soup = BeautifulSoup(channel, 'lxml')
+soup = BeautifulSoup(data, 'lxml')
 body = soup.find('body')
 titleFound = body.find_all(text=re.compile(passedProps["title"]))
 print(titleFound)
@@ -38,6 +43,20 @@ print(el)
 productSelector['title'] = f"{el.name}.{'.'.join(el['class'])}"
 print(productSelector)
 
+print(body)
+dateTest = body.find_all(text=re.compile(
+    'The draw date for this competition is\s+05/12/2021 8:00 pm'))
+
+print(dateTest)
+print(dateTest[0].parent)
+
+dateParseTest = 'The draw date for this competition is&nbsp;05/12/2021 8:00 pm'
+dateParseTest = dateParseTest.replace('&nbsp;', ' ')
+print(dateParseTest)
+drawDate = parse(dateParseTest, dayfirst=True, fuzzy=True)
+print(drawDate)
+print(drawDate.date())
+
 
 # TODO
 # Figure out best way to show a user which piece of information would be best
@@ -45,3 +64,6 @@ print(productSelector)
 # use static draw date times & calculate the time remaining client side
 # Determine whether to scrape the data of all product thumbnails first
 # or whether to scrape thumbnails & full product page at the same time
+
+# For dates etc where data from class selector is not only data needed
+# use CSS selector plus XPath with regex? Or just regex through class text
